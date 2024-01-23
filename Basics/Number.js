8ad748177eb6e59d40bd0394def21166cec5404c
Kundan Patil <146088477+YamrajOfCodes@users.noreply.github.com>
2024-01-23 06:38:10 +0000

    number and math

---
+++ Basics/Number.js
@@ -0,0 +1,35 @@
+// we can also define number using object
+
+// const num= new Number(100);  // these is a type of object
+// const num=100                   // these is a type of number
+
+// console.log(num);
+
+// // num.toString();
+// console.log( typeof num);
+
+// const str="hello";    // these are the type of string
+// const str2=new String('hello'); // these are the type of object
+
+// console.log(typeof str);
+
+
+
+              //         Math
+
+
+        //  let numbers=-4;
+        // console.log(Math.abs(numbers));
+         
+        //  const number2=45.12;
+        //  console.log(Math.round(number2)) // there are also ceil and floor ceil 45 ch gheil and floor 46
+
+
+
+                        // Random //
+          
+         const min=10;           // math.random he 0 and 1 madhe kahipn sankhya dete 
+         const max=20
+         console.log(Math.round(Math.random()*(max-min+1)+min));
+
+
