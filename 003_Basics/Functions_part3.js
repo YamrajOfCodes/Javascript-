92bd0f77effaeb0f85b22e627dc8b6db371255d1
Kundan Patil <146088477+YamrajOfCodes@users.noreply.github.com>
2024-01-26 06:07:02 +0000

    local and global scope

---
+++ 003_Basics/Functions_part3.js
@@ -0,0 +1,22 @@
+                                      // Global and Local scope
+
+ if(true){
+   
+// let b=20;
+// const c=30
+var a=10;
+
+  }
+
+
+//  console.log(a);
+//  console.log(b);
+ console.log(a);                       // the var also accessible here and that is a problem
+
+
+
+
+ // je {} madhe te local(block) scope but je {} chya baher dec.. te global scope
+
+
+
