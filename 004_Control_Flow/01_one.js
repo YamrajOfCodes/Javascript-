2e28a33bbf690a3b12b9948d736a8cb7891d7a01
Kundan Patil <146088477+YamrajOfCodes@users.noreply.github.com>
2024-01-28 12:40:31 +0000

    if else

---
+++ 004_Control_Flow/01_one.js
@@ -0,0 +1,10 @@
+                                              // IF and ELSE
+
+// there is a intermidiate type of notes here
+
+// 1) we can write if statement without curly braces but the condition is only in one line you can write mult line saperated by comma
+
+const temperature=40;
+
+if(temperature==40) console.log("temperature is 40 now");
+console.log("it is in control");
