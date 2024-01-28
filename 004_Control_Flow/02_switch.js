c258dcfc1460b98d7afd21cca17f07221babdbf5
Kundan Patil <146088477+YamrajOfCodes@users.noreply.github.com>
2024-01-28 12:40:46 +0000

    switch with keys

---
+++ 004_Control_Flow/02_switch.js
@@ -0,0 +1,50 @@
+
+const month=3;
+
+switch (month) {
+    case 1: console.log('January');
+        break;
+    case 2: console.log('Feb');
+        break;
+    case 3: console.log('March');
+        break;
+    case 4: console.log('April');
+        break;
+    case 5: console.log('May');
+        break;
+        case 6:console.log('June');
+        break;
+
+    default:
+        console.log('Another month');
+        break;
+}
+
+
+
+
+
+       // String
+
+
+
+       const months= "January"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
