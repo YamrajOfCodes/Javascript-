4a77a823172046a25cfa3e9407fd5d2a76ef00b3
Kundan Patil <146088477+YamrajOfCodes@users.noreply.github.com>
2024-01-29 13:30:10 +0000

    temporary code

---
+++ Practise/practise.js
@@ -0,0 +1,9 @@
+//The Object.create() static method creates a new object, 
+// using an existing object as the prototype of the newly created object.
+
+
+ const obj=new Object();
+
+ console.log(obj);
+
+  
